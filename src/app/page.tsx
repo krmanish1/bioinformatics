9a5d9f19dd13f73e150627467dd1d78f3f74a982
Manish Kumar <138682286+krmanish1@users.noreply.github.com>
2024-09-28 16:48:56 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+import Searchbar from "@/Components/Searchbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Searchbar />
+    </div>
+  );
+}
